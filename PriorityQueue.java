026b79e92cbc16751af98233d60482298bce0e87
Pedro Javier Marroquín Abrego <mar21801@uvg.edu.gt>
2022-04-07 07:40:22 -0600

    initial commit for program
    created main and interface for the priority queue

---
+++ PriorityQueue.java
@@ -0,0 +1,34 @@
+public interface PriorityQueue<E extends Comparable<E>> {
+    /**
+     *@return the minimum value in priority queue
+     * if the queue isn't empty
+     */
+    public E getFirst();
+
+    /**
+     *@return the minimum value in priority queue removing it as well
+     * if the queue isn't empty
+     */
+    public E remove();
+
+    /**
+     * @param value to add to Priority Queue
+     *value must be non-null and comparable
+     */
+    public void add(E value);
+
+    /**
+     * @return true if there aren't any values in the Priority Queue
+     */
+    public boolean isEmpty();
+
+    /**
+     * @return amount of values in the Priority Queue
+     */
+    public int size();
+
+    /**
+     * removes all elements from queue
+     */
+    public void clear();
+}
